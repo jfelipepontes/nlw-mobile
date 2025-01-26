9a18ca8403fd03b71e0e2917f099949e5cad93cd
jfelipepp <amazonfelipe1998@gmail.com>
2025-01-26 13:28:26 -0300

    Atualizações do notebook

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function Index () {
+    return (
+        <View>
+            <Text>Hello react</Text>
+        </View>
+    )
+}
+
+export default Index;
